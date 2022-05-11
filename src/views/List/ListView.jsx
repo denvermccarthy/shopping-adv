2b69024cb4b420eb0615fca92237374ffecb4e71
Denver McCarthy <dmichaelspdx@gmail.com>
2022-05-11 13:51:49 -0700

    user can add todos... I mean list items.

--- src/views/List/ListView.jsx
+++ src/views/List/ListView.jsx
@@ -1,19 +1,44 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
+import Item from '../../components/List/Item';
 
 const initialList = [{ id: Date.now(), done: false, name: 'banana' }];
-function reducer(state, action) {}
+function reducer(state, action) {
+  const { payload, type } = action;
+  switch (type) {
+    case 'ADD_ITEM':
+      return [payload.item, ...state];
+    default:
+      throw new Error(`${type} is not a valid type.`);
+  }
+}
 
 export default function ListView() {
   const [items, dispatch] = useReducer(reducer, initialList);
+  const [text, setText] = useState('');
   return (
     <div>
       ListView
-      <form>
-        <input type="text" placeholder="New Item" /> <button>ADD ITEM</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({
+            type: 'ADD_ITEM',
+            payload: { item: { id: Date.now(), done: false, name: text } },
+          });
+          setText('');
+        }}
+      >
+        <input
+          type="text"
+          placeholder="New Item"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />{' '}
+        <button>ADD ITEM</button>
       </form>
       <ul>
         {items.map((item) => (
-          <li>{item.name}</li>
+          <Item key={item.id} {...item} />
         ))}
       </ul>
     </div>

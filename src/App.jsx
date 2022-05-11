9e269f9ea7db6b92112a910f44c0ce7f1b2e5343
Denver McCarthy <dmichaelspdx@gmail.com>
2022-05-11 13:37:20 -0700

    todo rendering to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Header from './components/Layout/Header';
-import Input from './components/List/Input';
 import ListView from './views/List/ListView';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Input />
       <ListView />
     </>
   );
--- src/views/List/ListView.jsx
+++ src/views/List/ListView.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const initialList = [{ id: Date.now(), done: false, name: 'banana' }];
+function reducer(state, action) {}
 
 export default function ListView() {
-  return <div>ListView</div>;
+  const [items, dispatch] = useReducer(reducer, initialList);
+  return (
+    <div>
+      ListView
+      <form>
+        <input type="text" placeholder="New Item" /> <button>ADD ITEM</button>
+      </form>
+      <ul>
+        {items.map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
